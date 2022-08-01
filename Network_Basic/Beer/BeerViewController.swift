97139622a4fbd7bfa18059f2e87fe803ad04dfd6
wodyddml2 <wodyddml2@naver.com>
2022-08-01 16:25:08 +0900

    Beer API

---
+++ Network_Basic/Beer/BeerViewController.swift
@@ -0,0 +1,47 @@
+
+import UIKit
+
+import SwiftyJSON
+import Alamofire
+
+
+class BeerViewController: UIViewController {
+    @IBOutlet weak var beerLabel: UILabel!
+    @IBOutlet weak var beerImage: UIImageView!
+    @IBOutlet weak var beerDescription: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func requestRandomBeer() {
+        let url = "https://api.punkapi.com/v2/beers/random"
+        
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+                let imageURL = URL(string: json[0]["image_url"].string ?? "https://images.punkapi.com/v2/keg.png")
+                // try: 예외처리 구문(오류를 낼 수 있음에도 처리)
+                let imageData = try? Data(contentsOf: imageURL!)
+                
+                self.beerLabel.text = json[0]["name"].stringValue
+                self.beerImage.image = UIImage(data: imageData!)
+                
+                self.beerDescription.text = json[0]["description"].stringValue
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    
+    @IBAction func beerButtonClicked(_ sender: UIButton) {
+        requestRandomBeer()
+    }
+    
+}
